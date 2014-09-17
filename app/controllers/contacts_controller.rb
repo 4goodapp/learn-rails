ed7b922ce06efc42b1589f68d49055a5fecd1d49
4goodapp <4goodapp@gmail.com>
2014-09-17 05:44:36 +0000

    Finished Chapter 23 - sending mail

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,7 +9,8 @@ class ContactsController < ApplicationController
         if @contact.valid?
             #  save data to Google Drive 
             @contact.update_spreadsheet
-            # TODO send message
+            #  send email message
+            UserMailer.contact_email(@contact).deliver
             flash[:notice] = "Message sent from #{@contact.name}."
             redirect_to root_path
         else
